6afe34475fda5ca8a74ee305c404c6a3eed51206
Mansur <Mansur@COMPUTER>
2022-11-21 17:15:30 +0500

    Add project files.

---
+++ Testing_IHostedService/MyJobTasks/TimedHostedService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testing_IHostedService.MyJobTasks
+{
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;        
+        private Timer? _timer = null;
+        private string _taskName;
+
+        private TimeSpan _period;
+
+        public TimedHostedService()
+        {
+            //Default value:
+            _period = TimeSpan.FromSeconds(1);
+        }
+
+        public TimedHostedService(string taskName, TimeSpan period)
+        {
+            _period = period;
+            _taskName = taskName;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine($"{_taskName}  task running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, _period);
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            var count = Interlocked.Increment(ref executionCount);
+
+            Console.WriteLine($"Timed Hosted Service's DoWork method is working. Count: {count}, Task name:{_taskName}");
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
---
+++ Testing_IHostedService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Testing_IHostedService.MyJobTasks;
+
+namespace Testing_IHostedService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var tasks = new List<TimedHostedService>();
+            for (var i = 0; i < 100; i++)
+            {
+                tasks.Add(new TimedHostedService($"task{i}", TimeSpan.FromSeconds(0.1)));
+            }
+            foreach (var item in tasks)
+            {
+                var ct = new CancellationToken();
+                await item.StartAsync(ct);
+            }
+            Console.ReadLine();
+        }
+    }
+}
